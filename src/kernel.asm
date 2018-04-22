e8fa653fa17bf11fe7752d0214f45ef4aec8f514
MijnOS <36691585+MijnOS@users.noreply.github.com>
2018-04-22 17:08:16 +0200

    kernel fat12 search entry

--- src/kernel.asm
+++ src/kernel.asm
@@ -9,15 +9,16 @@ kernel:
     mov     ax,cs
     mov     ds,ax
     mov     es,ax
-    add     ax,100h     ; 4096 - Means the kernel may be up to 4096-bytes in size
+    add     ax,100h
     mov     ss,ax
-    mov     sp,8000h    ; 32kb - This is the available stack space
+    mov     sp,8000h
+    ;mov     ss,ax
+    ;mov     sp,9000h
+    ;  4kb - 1000h - This is the maximum size of the kernel.
+    ; 32kb - 8000h - This is the available stack space
 
-    mov     si,msg_success
-    call    print
-
-    call    test_sant
-    call    print_newline
+    ;call    test_loadSector
+    ;call    print_newline
 
     call    test_search
     call    print_newline
@@ -26,51 +27,17 @@ kernel:
 
 
 ;===========
-; void print(const char *si)
-;   @param si A pointer to the string to print.
-;===========
-print:
-    mov     ah,0Eh
-
-.repeat:
-    lodsb
-    cmp     al,0
-    je      .return
-    int     10h
-    jmp     .repeat
-
-.return:
-    ret
-
-;===========
 ; STRINGS
 ;===========
 msg_success db "Kernel reports 0 errors." ,0Dh,0Ah,0
-str_sant    db "sdfj0ßäö-_0ß34", 0Dh, 0Ah, 0
-.length     equ ($-str_sant)
-
-
-test_sant:
-    push    bp
-    mov     bp,sp
-    sub     sp,str_sant.length
-
-    lea     ax,[bp-str_sant.length]
-    mov     di,ax
-    mov     si,str_sant
-    call    fat_sanitize
-
-    mov     si,di
-    call    print
-
-    mov     sp,bp
-    pop     bp
-    ret
-
+msg_failed  db "Kernel reports 1 error." ,0Dh,0Ah,0
+.length     equ ($-msg_success)
 
 file_cmd    db 'CMD     BIN' 
 cmd_found   db 'CMD found', 0Dh, 0Ah, 0
 cmd_nfound  db 'CMD not found', 0Dh, 0Ah, 0
+
+str_called  db 'CALLED', 0Dh, 0Ah, 0
 
 
 test_search:
@@ -80,7 +47,21 @@ test_search:
 
     lea     di,[bp-32]
     mov     si,file_cmd
+
+    push    es
+    push    bx
+
+    mov     bx,ss
+    mov     es,bx
+
+    push    si
+    push    di
     call    fat_searchEntry
+    add     sp,4
+
+    pop     bx
+    pop     es
+
     test    ax,ax
     jne     .not_found
 
@@ -88,7 +69,19 @@ test_search:
     call    print
     call    print_newline
 
-    mov     ax,word [bp-6]
+    push    ds
+    push    bx
+    mov     bx,ss
+    mov     ds,bx
+    mov     word [bp-21],0
+    lea     ax,[bp-32]
+    mov     si,ax
+    call    print
+    call    print_newline
+    pop     bx
+    pop     ds
+
+    mov     ax,word [bp-6]          ; 0500h
     call    print_hex
     call    print_newline
 
@@ -105,6 +98,43 @@ test_search:
     ret
 
 
+test_loadSector:
+    push    bp
+    mov     bp,sp
+    sub     sp,512
+    push    di
 
-%include "src/kernel/string.inc"
+    push    es
+    push    bx
+
+    mov     bx,ss
+    mov     es,bx
+
+    lea     di,[bp-512]
+    mov     ax,0
+    call    fat_loadRootSector
+
+    pop     bx
+    pop     es
+
+    mov     ax,word [bp-512]
+    call    print_hex
+
+    mov     ax,word [bp-480]
+    call    print_hex
+
+    mov     ax,word [bp-448]
+    call    print_hex
+
+    mov     ax,word [bp-416]
+    call    print_hex
+
+    pop     di
+    mov     sp,bp
+    pop     bp
+    ret
+
+
+
+%include "src/kernel/std.inc"
 %include "src/kernel/fat12.inc"
